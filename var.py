23b528e6af8fc44de74dfddde147a73c54353afb
Ndegwadavid <davidndegwa6869@gmail.com>
2022-12-06 04:29:43 -0500

    more and more py basics

--- var.py
+++ var.py
@@ -9,3 +9,10 @@ print(y)
 #comments in python are indented with the #value
 
 print("good morning my people") #this is a comment 
+
+
+#a case where a variable changes with immediate even if they had been set
+x = 4    # x is a type of int
+x = "sally" #x is now a type of string 
+print(x)
+#this case the value printed will be sally 
