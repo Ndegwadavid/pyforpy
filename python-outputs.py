714bec34aa2d4d62d4d6bba005fbf40a023b80da
Ndegwadavid <davidndegwa6869@gmail.com>
2022-12-06 05:17:58 -0500

    ouputs for python are very simple

---
+++ python-outputs.py
@@ -0,0 +1,28 @@
+#the ways to print outputs in python
+#method 1
+x = 'python is awesome'
+print(x)
+
+#method 2
+x = 'python'
+y = 'is'
+z = 'awesome'
+print(x,y,z)
+#method3 using the plus sign
+x = 'python '
+y = 'is '
+z = 'awesome '
+print(x+y+z)
+
+#calculations with python
+x = 5
+y = 10
+print(x + y)
+
+#a string and an int will never meger lest they will lead to errors
+x =5
+y = 'John'
+print(x+y) #this will lead to errors but can be done when separated by a comma 
+x=5
+y = 'john'
+print(x, y) #this code will work
