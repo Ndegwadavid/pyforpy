e95ac3b3f63317bd6c7e784875df1d493c0ac7e3
Ndegwadavid <davidndegwa6869@gmail.com>
2022-12-06 02:31:33 -0500

    second one showing comments for python

---
+++ var.py
@@ -0,0 +1,11 @@
+x = 5
+y = "Hello world"
+
+print(x)
+print(y)
+
+#print("good morning my neighbours")
+
+#comments in python are indented with the #value
+
+print("good morning my people") #this is a comment 
