30813a619f8f1a9f6deeb45a14d8557df438d4a3
Ndegwadavid <davidndegwa6869@gmail.com>
2022-12-06 04:56:03 -0500

    assigning values

---
+++ multiple-vars.py
@@ -0,0 +1,21 @@
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+#one value to multiple variables
+x = y = z = "orange"
+print(x)
+print(y)
+print(z)
+     #the values of x, y,z, wll be the same 
+
+     #unpacking values using python
+fruits = ["apple", 'banana', 'cherry']
+x, y, z = fruits
+
+print(x)
+print(y)
+print(z)
+
+
