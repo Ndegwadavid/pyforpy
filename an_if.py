8cc36745aefb814495dad0ee27bd46ac401e4dc8
Ndegwadavid <davidndegwa6869@gmail.com>
2022-12-07 02:26:45 -0500

    the first if

---
+++ an_if.py
@@ -0,0 +1,16 @@
+#to check if there is a string in a paragraph
+txt = "The best things in life are free!"
+if "free" in txt:
+   print("Yes.")
+
+txt = "The best things in life are free!"
+print( "expensive" not in txt)
+
+#slicing python strings
+#Get the characters from position 2 to position 5 (not included):
+b = "Hello, World!"
+print(b[2:5])
+
+#negative indexing 
+b = "Hello you whats your job here"
+print(b[-5:-2])
