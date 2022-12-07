048aabb432d936ce7dfee107ce6b274967deec06
Ndegwadavid <davidndegwa6869@gmail.com>
2022-12-07 01:55:07 -0500

    string that abit for me

---
+++ getstringcharacter-array.py
@@ -0,0 +1,20 @@
+#the first character in python lang has ussually the position zero
+a = 'hello'
+print(a[1]) #this will print (e) hence its the following after zero 
+
+
+#looping through a string
+#since strings are arrays we can loop though the characters using the for loop
+for x in "banana":
+    #print(x)
+
+#to print the length of charcters we will be using the len attribute
+a = "Hello world"
+print(len(a))
+
+#to check the string in acharcater or a text
+a = "The best things in life are free!"
+print("free" in a)  #this will return a true value if it gets the value in the string provided
+
+
+
