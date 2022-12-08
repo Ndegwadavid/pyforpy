a9698db6bae39741faa803f3225b4b4489a50b00
Ndegwadavid <davidndegwa6869@gmail.com>
2022-12-07 21:28:28 -0500

    strings and thier stuffs

---
+++ morepy.py
@@ -0,0 +1,40 @@
+#uppercase in python
+a = "Hello, World!"
+print(a.upper()) #this prints the output in capital letters
+
+#lowercase
+a = "Hello, World!"
+print(a.lower()) #this prints the output in small letters
+
+#remove whitespaces in python
+a = " Hello, World! "
+print(a.strip()) 
+
+#replacing a string 
+a = "Hello, World!"
+print(a.replace("H", "J"))  #this one here replaces the h in hello world 
+
+#split a string 
+a = "Hello, World!"
+print(a.split(","))
+
+#string adding(concatenation)
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+
+#adding as space between the concatenated string
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+#escape characters in python .....when we want to solely identify a string
+
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt)  #this will make the output of the word vikings be in quotation marks through the power of the backslash
+
+
+
